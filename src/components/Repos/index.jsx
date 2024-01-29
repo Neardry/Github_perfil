ba85296c54aacfe862436493c1aa90328a4fda33
Neardry <brunodossantosportela@gmail.com>
2024-01-28 21:02:10 -0300

    Adicionando mensagem de erro

--- src/components/Repos/index.jsx
+++ src/components/Repos/index.jsx
@@ -6,20 +6,34 @@ const Repos = ({ userName }) => {
     const ENDPOINT = `https://api.github.com/users/${userName}/repos`;
     const [repos, setRepos] = useState([]);
     const [load, setLoad] = useState(true);
+    const [loadError, setLoadError] = useState(false)
     useEffect(() => {
-        setLoad(true)
-            fetch(ENDPOINT).then(res => res.json().then(resJson => {
-                setTimeout(() => {
-                    setLoad(false);
-                    setRepos(resJson)}, 3000)
-                }))
-    }, [userName]); //precisa colocar aqui para ele conseguir renderizar o nome do usuario primeiro
+        setLoad(true);
+        fetch(ENDPOINT)
+        .then(res => {
+            if (!res.ok) {
+            throw new Error(`Erro ao buscar o repositório (${res.status} ${res.statusText})`); //Meio que é obrigado a utilizar essa linha para o catch() funcionar
+            }
+            return res.json();
+        })
+        .then(resJson => {
+            setTimeout(() => {
+            setLoad(false);
+            setLoadError(false);
+            setRepos(resJson);
+            }, 3000);
+        })
+        .catch((e) => {
+            setLoadError(true);
+            setLoad(false);
+            });
+      }, [userName]); //precisa colocar aqui para ele conseguir renderizar o nome do usuario primeiro
     return (
         <div className='container'>
-            {load ? (
+            {load || loadError ? (
                 <h3>Carregando...</h3>
-            ): (
-<>
+                ): (
+                    <>
             <li>Contagem de repositórios: {repos.length}</li>
             <br />
             <br />
@@ -41,6 +55,7 @@ const Repos = ({ userName }) => {
         </ul>
                 </>
             )}
+    {loadError && (<p className={styles.error}>Erro ao buscar o repositório, verifique o valor no campo digitado</p>)}
         </div>
     )
 };
